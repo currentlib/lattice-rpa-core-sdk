5ff66775722a3dfccf8e3e25ebf10d867d6a21c2
currentlib <y.tymkiv.official@gmail.com>
2026-07-24 12:33:42 +0300

    build: bump setup version to 0.4.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rpa_core",
-    version="0.4.0",
+    version="0.4.2",
     description="Enterprise Python SDK with BaseDispatcher & BasePerformer Frameworks for Lattice RPA Orchestrator",
     author="Lattice Team",
     packages=find_packages(),
